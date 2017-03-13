a1190a011bc7cc6c6972ed2a3f3326a3cb2e5c15
Kraisorn Arjharn <kraisorna@aol.com>
2017-03-13 18:35:51 +0700

    First local commit

---
+++ PermMissingElem.cpp
@@ -0,0 +1,15 @@
+// PermMissingElem.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a;
+	cin >> a;
+    return 0;
+}
+
